f9c604075b6b93dd3403fd4b05d028da92917c10
Lei Niu <lei.niu.ny@gmail.com>
2020-07-01 21:53:58 -0400

    Update index.ts

--- apps/index.ts
+++ apps/index.ts
@@ -1,3 +1,14 @@
 import * as taos from 'td-connector';
-const conn = taos.connect({ host: "127.0.0.1", user: "root", password: "taosdata", config: "/etc/taos", port: 0 })
+const host = 'tdengine';
+const user = 'root';
+const password = 'taosdata';
+const config = '/etc/taos';
+const port = 6020;
+const conn = taos.connect({
+    host,
+    user,
+    password,
+    config,
+    port
+})
 const cursor = conn.cursor();
